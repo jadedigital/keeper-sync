b19f51175de261b71a378ae6661f08392d2226fb
Aaron Greene <AGreene@technip.com>
2018-02-05 17:02:27 -0330

    ui updates

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,6 +1,19 @@
 <template>
   <q-pull-to-refresh :handler="refresher" class="chat-layout">
-    Chat
+    <q-list highlight>
+      <q-item 
+        v-for="chat in franchiseList" 
+        :key="chat.id"
+      >
+        <q-item-side :avatar="chat.icon ? chat.icon : './statics/avatar.jpg'" />
+        <q-item-main :label="chat.name" :sublabel="chatList[chat.id][0] ? chatList[chat.id][0].message : ''" />
+        <q-item-side right>
+          <q-item-tile>
+            {{chatList[chat.id][0] ? chatList[chat.id][0].posted.substring(0, 5) : ''}}
+          </q-item-tile>
+        </q-item-side>
+      </q-item>
+    </q-list>
   </q-pull-to-refresh>
 </template>
 
@@ -32,7 +45,7 @@ import bTeam from './bTeam.vue'
 import bSpinner from './bSpinner.vue'
 
 export default {
-  name: 'team',
+  name: 'chat',
   components: {
     QBtn,
     QList,
@@ -78,6 +91,49 @@ export default {
     teamLookup () {
       var array = this.league.franchises.franchise
       return this.lookup(array, 'id')
+    },
+    franchiseList () {
+      var commish = {
+        id: '0000',
+        name: 'Commissioner'
+      }
+      var league = {
+        id: '1000',
+        name: 'League Chat'
+      }
+      const franchiseArray = [...this.league.franchises.franchise]
+      franchiseArray.unshift(commish)
+      franchiseArray.unshift(league)
+      franchiseArray.splice(franchiseArray.findIndex(this.matchesEl), 1)
+      return franchiseArray
+    },
+    chatList () {
+      var chatList = {}
+      var chatArray = []
+      var commish = {
+        id: '0000',
+        name: 'Commissioner'
+      }
+      const franchiseArray = [...this.league.franchises.franchise]
+      franchiseArray.unshift(commish)
+      franchiseArray.forEach(el => {
+        this.chat.message.forEach(el2 => {
+          if (el2._attributes.to && (el2._attributes.franchise_id === el.id || el2._attributes.to === el.id)) {
+            chatArray.push(el2._attributes)
+          }
+        })
+        chatList[el.id] = chatArray
+        chatArray = []
+      })
+
+      this.chat.message.forEach(el => {
+        if (!el._attributes.to) {
+          chatArray.push(el._attributes)
+        }
+      })
+      chatList['1000'] = chatArray
+
+      return chatList
     }
   },
   methods: {
@@ -87,6 +143,9 @@ export default {
         lookup[array[i][key]] = array[i]
       }
       return lookup
+    },
+    matchesEl (el) {
+      return el.id === this.myTeam
     },
     refresher (done) {
       var host = this.leagueData[this.activeLeague].host
